da1661c056177bfae86eedf2496169a2aeb66358
Ildarc4S <ildarc4S@gmail.com>
2025-04-25 11:09:44 +0300

    Begin write map

--- s21_map.h
+++ s21_map.h
@@ -1,15 +1,19 @@
 #ifndef S21_MAP_H
 #define S21_MAP_H
 
+#include <functional>
+#include <memory>
+
 namespace s21 {
 
 /*
  * @class map
  * @brief Реализация контейнера map аналогичного std::map
  */
-template <typename T>
+template <typename Key_, typename Value_, typename Compare_ = std::less<Key_>,
+          typename Alloc_ = std::allocator<std::pair<const Key_, Value_>>>
 class map {
-  // Реализация контейнера map
+
 };
 
 }  // namespace s21
