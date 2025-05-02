297db1b704633d3bd49dc64c730bb6243967d754
austinma <austinma@student.21-school.ru>
2025-05-02 11:44:37 +0300

    maybe done vector

--- s21_vector.h
+++ s21_vector.h
@@ -1,15 +1,399 @@
 #ifndef S21_VECTOR_H
 #define S21_VECTOR_H
 
+#include <iostream>
+#include <limits>
+
 namespace s21 {
 
 /*
  * @class vector
  * @brief Реализация контейнера vector аналогичного std::vector
  */
-template <typename T>
+template <typename T, typename Alloc = std::allocator<T>>
 class vector {
-  // Реализация контейнера vector
+ public:
+  class VectorIterator {
+   private:
+    T* ptr;
+
+   public:
+    explicit VectorIterator(T* pointer) : ptr(pointer) {}
+
+    T& operator*() { return *ptr; }
+
+    VectorIterator& operator++() {
+      ++ptr;
+      return *this;
+    }
+
+    VectorIterator operator++(int) {
+      VectorIterator tmp = *this;
+      ++ptr;
+      return tmp;
+    }
+
+    VectorIterator& operator--() {
+      --ptr;
+      return *this;
+    }
+
+    VectorIterator operator--(int) {
+      VectorIterator tmp = *this;
+      --ptr;
+      return tmp;
+    }
+
+    bool operator==(const VectorIterator& other) const {
+      return ptr == other.ptr;
+    }
+    bool operator!=(const VectorIterator& other) const {
+      return ptr != other.ptr;
+    }
+
+    T& operator[](int index) { return *(ptr + index); }
+
+    VectorIterator operator+(int n) const { return VectorIterator(ptr + n); }
+
+    VectorIterator operator-(int n) const { return VectorIterator(ptr - n); }
+    ptrdiff_t operator-(const VectorIterator& other) const {
+      return ptr - other.ptr;
+    }
+
+    bool operator>(const VectorIterator& other) const {
+      return ptr > other.ptr;
+    }
+    bool operator<(const VectorIterator& other) const {
+      return ptr < other.ptr;
+    }
+    bool operator>=(const VectorIterator& other) const {
+      return ptr >= other.ptr;
+    }
+    bool operator<=(const VectorIterator& other) const {
+      return ptr <= other.ptr;
+    }
+  };
+
+  class VectorIteratorConst {
+   private:
+    const T* ptr;
+
+   public:
+    explicit VectorIteratorConst(const T* pointer) : ptr(pointer) {}
+
+    const T& operator*() { return *ptr; }
+
+    VectorIteratorConst& operator++() {
+      ++ptr;
+      return *this;
+    }
+
+    VectorIteratorConst operator++(int) {
+      VectorIteratorConst tmp = *this;
+      ++ptr;
+      return tmp;
+    }
+
+    VectorIteratorConst& operator--() {
+      --ptr;
+      return *this;
+    }
+
+    VectorIteratorConst operator--(int) {
+      VectorIteratorConst tmp = *this;
+      --ptr;
+      return tmp;
+    }
+
+    bool operator==(const VectorIteratorConst& other) const {
+      return ptr == other.ptr;
+    }
+    bool operator!=(const VectorIteratorConst& other) const {
+      return ptr != other.ptr;
+    }
+
+    const T& operator[](int index) { return *(ptr + index); }
+
+    VectorIteratorConst operator+(int n) const {
+      return VectorIteratorConst(ptr + n);
+    }
+
+    VectorIteratorConst operator-(int n) const {
+      return VectorIteratorConst(ptr - n);
+    }
+    ptrdiff_t operator-(const VectorIteratorConst& other) const {
+      return ptr - other.ptr;
+    }
+
+    bool operator>(const VectorIteratorConst& other) const {
+      return ptr > other.ptr;
+    }
+    bool operator<(const VectorIteratorConst& other) const {
+      return ptr < other.ptr;
+    }
+    bool operator>=(const VectorIteratorConst& other) const {
+      return ptr >= other.ptr;
+    }
+    bool operator<=(const VectorIteratorConst& other) const {
+      return ptr <= other.ptr;
+    }
+  };
+
+  using value_type = T; 
+  using reference = T&;
+  using const_reference = const T&;
+  using iterator = VectorIterator;
+  using const_iterator = VectorIteratorConst;
+  using size_type = size_t;
+  using traits = std::allocator_traits<Alloc>;
+
+  explicit vector() : data_(nullptr), size_(0), capacity_(0) {}
+
+  explicit vector(size_type n) : data_(nullptr), size_(n), capacity_(n) {
+    data_ = traits::allocate(alloc, capacity_);
+    try {
+      for (size_type i = 0; i < size_; ++i) {
+        traits::construct(alloc, data_ + i);
+      }
+    } catch (...) {
+      for (size_type j = 0; j < size_; ++j) {
+        traits::destroy(alloc, data_ + j);
+      }
+      traits::deallocate(alloc, data_, capacity_);
+      throw;
+    }
+  }
+
+  vector(std::initializer_list<value_type> const& items)
+      : data_(nullptr), size_(items.size()), capacity_(items.size()) {
+    data_ = traits::allocate(alloc, capacity_);
+    size_type index = 0;
+    try {
+      auto iterItem = items.begin();
+      for (; iterItem != items.end(); ++index) {
+        traits::construct(alloc, data_ + index, *iterItem);
+        ++iterItem;
+      }
+    } catch (...) {
+      for (size_type j = 0; j < index; ++j) {
+        traits::destroy(alloc, data_ + j);
+      }
+      traits::deallocate(alloc, data_, capacity_);
+      throw;
+    }
+  }
+
+  vector(const vector& other)
+      : data_(nullptr), size_(other.size_), capacity_(other.size_) {
+    data_ = traits::allocate(alloc, capacity_);
+    size_type index = 0;
+    try {
+      for (; index < size_; ++index) {
+        traits::construct(alloc, data_ + index, other.data_[index]);
+      }
+    } catch (...) {
+      for (size_type j = 0; j < index; ++j) {
+        traits::destroy(alloc, data_ + j);
+      }
+      traits::deallocate(alloc, data_, capacity_);
+      throw;
+    }
+  }
+
+  vector(vector&& other) noexcept
+      : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
+    other.data_ = nullptr;
+    other.size_ = 0;
+    other.capacity_ = 0;
+  }
+
+  ~vector() {
+    for (size_type i = 0; i < size_; ++i) {
+      traits::destroy(alloc, data_ + i);
+    }
+
+    if (data_) {
+      traits::deallocate(alloc, data_, capacity_);
+    }
+  }
+
+  vector& operator=(vector&& other) noexcept {
+    if (this != &other) {
+      for (size_type i = 0; i < size_; ++i) {
+        traits::destroy(alloc, data_ + i);
+      }
+
+      if (data_) {
+        traits::deallocate(alloc, data_, capacity_);
+      }
+
+      data_ = other.data_;
+      size_ = other.size_;
+      capacity_ = other.capacity_;
+
+      other.data_ = nullptr;
+      other.size_ = 0;
+      other.capacity_ = 0;
+    }
+    return *this;
+  }
+
+  reference at(size_type pos) {
+    if (size_ <= pos) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[pos];
+  }
+
+  [[nodiscard]] reference at(size_type pos) const {
+    if (size_ <= pos) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[pos];
+  }
+
+  reference operator[](size_type pos) { return data_[pos]; }
+
+  const_reference operator[](size_type pos) const { return data_[pos]; }
+
+  const_reference front() { return data_[0]; }
+
+  const_reference back() { return data_[size_ - 1]; }
+
+  T* data() { return data_; }
+
+  iterator begin() { return iterator(data_); }
+
+  iterator end() { return iterator(data_ + size_); }
+
+  [[nodiscard]] const_iterator cbegin() const { return const_iterator(data_); }
+
+  [[nodiscard]] const_iterator cend() const { return const_iterator(data_ + size_); }
+
+  [[nodiscard]]bool empty() const { return size_ == 0; }
+
+  [[nodiscard]]size_type size() const { return size_; }
+
+  [[nodiscard]]size_type max_size() const {
+    return std::numeric_limits<size_type>::max() / sizeof(T);
+  }
+
+  void reserve(size_type capacity) {
+    if (capacity > capacity_){
+      T* newdata = traits::allocate(alloc, capacity);
+    try {
+      for (size_type i = 0; i < size_; ++i) {
+        traits::construct(alloc, newdata + i, data_[i]);
+      }
+    } catch (...) {
+      traits::deallocate(alloc, newdata, capacity);
+      throw;
+    }
+
+    for (size_type i = 0; i < size_; ++i) {
+      traits::destroy(alloc, data_ + i);
+    }
+    traits::deallocate(alloc, data_, capacity_);
+    data_ = newdata;
+    capacity_ = capacity;
+    }
+    
+  }
+
+  size_type capacity() { return capacity_; }
+
+  void shrink_to_fit() {
+    if (size_ != capacity_){
+      T* newData = traits::allocate(alloc, size_);
+      for (size_type i = 0; i < size_; ++i) {
+        traits::construct(alloc, newData + i, std::move(data_[i]));
+        traits::destroy(alloc, data_ + i);
+      }
+
+      if (data_) {
+        traits::deallocate(alloc, data_, capacity_);
+      }
+
+      data_ = newData;
+      capacity_ = size_;
+    }
+
+    
+  }
+
+  void clear() {
+    for (size_type i = 0; i < size_; ++i) {
+      traits::destroy(alloc, data_ + i);
+    }
+    size_ = 0;
+  }
+
+  void push_back(const_reference value) {
+    if (size_ == capacity_) {
+      reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+    traits::construct(alloc, data_ + size_, value);
+    size_++;
+  }
+
+  void pop_back() {
+    if (size_ == 0) {
+      throw std::out_of_range("OUT OF RANGE");
+    }
+    traits::destroy(alloc, data_ + size_ - 1);
+    size_--;
+  }
+
+  iterator insert(iterator pos, const_reference value) {
+    size_type index = pos - begin();
+
+    if (size_ == capacity_) {
+      reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+
+    size_type position = size_;
+    try {
+      for (; position > index; --position) {
+        traits::construct(alloc, data_ + position, data_[position - 1]);
+      }
+      traits::construct(alloc, data_ + index, value);
+    } catch (...) {
+      for (size_type j = size_; j > position; --j) {
+        traits::destroy(alloc, data_ + j);
+      }
+      throw;
+    }
+
+    size_++;
+    return iterator(data_ + index);
+  }
+  // вопрос на что ориентироваться!!! по разному возвращаемые значения
+  void erase(iterator pos) {
+    size_type index = pos - begin();
+
+    traits::destroy(alloc, data_ + index);
+
+    for (size_type i = index; i < size_ - 1; ++i) {
+      traits::construct(alloc, data_ + i, std::move(data_[i + 1]));
+      traits::destroy(alloc, data_ + i + 1);
+    }
+
+    size_--;
+  }
+
+  void swap(vector& other) noexcept{
+    using std::swap;
+    swap(data_, other.data_);
+    swap(size_, other.size_);
+    swap(capacity_, other.capacity_);
+    swap(alloc, other.alloc);
+  }
+
+ private:
+  T* data_;
+  size_type size_;
+  size_type capacity_;
+  Alloc alloc;
 };
 
 }  // namespace s21
--- tests/s21_test_vector.cc
+++ tests/s21_test_vector.cc
@@ -5,9 +5,350 @@
 // TODO(root): Add test cases for vector
 
 TEST(vectorTest, DefaultConstructor) {
-  // Test default constructor
+  s21::vector<int> victor;
+  EXPECT_EQ(victor.size(), 0);
+  EXPECT_EQ(victor.capacity(), 0);
+}
+
+TEST(vectorTest, SizedConstructor) {
+  s21::vector<int> victor(5);
+  EXPECT_EQ(victor.size(), 5);
+  EXPECT_EQ(victor.capacity(), 5);
 }
 
 TEST(vectorTest, InitializerListConstructor) {
-  // Test initializer list constructor
+  s21::vector<int> victor{1, 2, 3, 4};
+  EXPECT_EQ(victor.size(), 4);
+  EXPECT_EQ(victor[0], 1);
+  EXPECT_EQ(victor[1], 2);
+  EXPECT_EQ(victor[2], 3);
+  EXPECT_EQ(victor[3], 4);
+}
+
+TEST(vectorTest, CopyConstructor) {
+  s21::vector<int> zictor{1, 2, 3, 4};
+  s21::vector<int> victor(zictor);
+  EXPECT_EQ(victor.size(), 4);
+  EXPECT_EQ(victor[0], 1);
+  EXPECT_EQ(victor[1], 2);
+  EXPECT_EQ(victor[2], 3);
+  EXPECT_EQ(victor[3], 4);
+}
+
+TEST(vectorTest, MoveConstructor) {
+  s21::vector<int> zictor{1, 2, 3, 4};
+  s21::vector<int> victor(std::move(zictor));
+  EXPECT_EQ(victor.size(), 4);
+  EXPECT_EQ(victor[0], 1);
+  EXPECT_EQ(victor[1], 2);
+  EXPECT_EQ(victor[2], 3);
+  EXPECT_EQ(victor[3], 4);
+  EXPECT_EQ(zictor.size(), 0);
+  EXPECT_EQ(zictor.capacity(), 0);
+}
+
+TEST(vectorTest, MoveEqConstructor) {
+  s21::vector<int> zictor{1, 2, 3, 4};
+  s21::vector<int> victor;
+  victor = std::move(zictor);
+  EXPECT_EQ(victor.size(), 4);
+  EXPECT_EQ(victor[0], 1);
+  EXPECT_EQ(victor[1], 2);
+  EXPECT_EQ(victor[2], 3);
+  EXPECT_EQ(victor[3], 4);
+  EXPECT_EQ(zictor.size(), 0);
+  EXPECT_EQ(zictor.capacity(), 0);
+}
+
+TEST(vectorTest, AtValidIndex) {
+  s21::vector<int> victor = {10, 20, 30};
+  EXPECT_EQ(victor.at(0), 10);
+  EXPECT_EQ(victor.at(1), 20);
+  EXPECT_EQ(victor.at(2), 30);
+}
+
+TEST(vectorTest, AtInvalidIndexThrows) {
+  s21::vector<int> victor = {1, 2, 3};
+  EXPECT_THROW(victor.at(3), std::out_of_range);
+  EXPECT_THROW(victor.at(100), std::out_of_range);
+}
+
+TEST(VectorTest, ConstAtWorksCorrectly) {
+  const s21::vector<int> vec = {5, 10, 15};
+  EXPECT_EQ(vec.at(0), 5);
+  EXPECT_EQ(vec.at(1), 10);
+  EXPECT_EQ(vec.at(2), 15);
+}
+
+TEST(VectorTest, ConstAtInvalidIndexThrows) {
+  const s21::vector<int> vec = {7, 8};
+  EXPECT_THROW((void)vec.at(2), std::out_of_range);
+}
+
+TEST(vectorTest, OperatorSquareBrackets) {
+  s21::vector<int> victor = {5, 10, 15};
+  EXPECT_EQ(victor[0], 5);
+  EXPECT_EQ(victor[1], 10);
+  EXPECT_EQ(victor[2], 15);
+  victor[1] = 42;
+  EXPECT_EQ(victor[1], 42);
+}
+
+TEST(vectorTest, FrontBackData) {
+  s21::vector<int> victor = {100, 200, 300};
+  EXPECT_EQ(victor.front(), 100);
+  EXPECT_EQ(victor.back(), 300);
+  EXPECT_EQ(victor.data()[0], 100);
+}
+
+TEST(vectorTest, Iterators) {
+  s21::vector<int> victor = {1, 2, 3};
+  auto itVector = victor.begin();
+  EXPECT_EQ(*itVector, 1);
+  ++itVector;
+  EXPECT_EQ(*itVector, 2);
+  auto end = victor.end();
+  --end;
+  EXPECT_EQ(*end, 3);
+}
+
+TEST(vectorTest, ConstIterators) {
+  const s21::vector<int> victor = {11, 22, 33};
+  auto itVector = victor.cbegin();
+  EXPECT_EQ(*itVector, 11);
+  ++itVector;
+  EXPECT_EQ(*itVector, 22);
+  auto end = victor.cend();
+  --end;
+  EXPECT_EQ(*end, 33);
+}
+
+TEST(vectorTest, EmptyAndSize) {
+  s21::vector<int> empty_victor;
+  EXPECT_TRUE(empty_victor.empty());
+  EXPECT_EQ(empty_victor.size(), 0);
+
+  s21::vector<int> victor(4);
+  EXPECT_FALSE(victor.empty());
+  EXPECT_EQ(victor.size(), 4);
+}
+
+TEST(vectorTest, MaxSize) {
+  s21::vector<int> victor;
+  EXPECT_GT(victor.max_size(), 1000);
+}
+
+TEST(VectorTest, ReserveIncreasesCapacity) {
+  s21::vector<int> victor;
+  victor.reserve(10);
+  EXPECT_GE(victor.capacity(), 10);
+}
+
+TEST(VectorTest, ShrinkToFitReducesCapacity) {
+  s21::vector<int> victor;
+  for (int i = 0; i < 10; ++i){
+    victor.push_back(i);
+  } 
+  victor.reserve(20);
+  EXPECT_GT(victor.capacity(), victor.size());
+  victor.shrink_to_fit();
+  EXPECT_EQ(victor.capacity(), victor.size());
+}
+
+TEST(VectorTest, ClearEmptiesVector) {
+  s21::vector<int> victor{1, 2, 3};
+  victor.clear();
+  EXPECT_EQ(victor.size(), 0);
+  EXPECT_GE(victor.capacity(), 3);
+}
+
+TEST(VectorTest, PushBackAddsElements) {
+  s21::vector<int> victor;
+  victor.push_back(42);
+  EXPECT_EQ(victor.size(), 1);
+  EXPECT_EQ(victor[0], 42);
+}
+
+TEST(VectorTest, PopBackRemovesLastElement) {
+  s21::vector<int> victor{1, 2, 3};
+  victor.pop_back();
+  EXPECT_EQ(victor.size(), 2);
+  EXPECT_EQ(victor.back(), 2);
+}
+
+TEST(VectorTest, PopBackThrowsWhenEmpty) {
+  s21::vector<int> victor;
+  EXPECT_THROW(victor.pop_back(), std::out_of_range);
+}
+
+TEST(VectorTest, InsertAtBeginning) {
+  s21::vector<int> victor{2, 3};
+  victor.insert(victor.begin(), 1);
+  EXPECT_EQ(victor[0], 1);
+  EXPECT_EQ(victor.size(), 3);
+}
+
+TEST(VectorTest, InsertInMiddle) {
+  s21::vector<int> victor{1, 3};
+  victor.insert(victor.begin() + 1, 2);
+  EXPECT_EQ(victor[1], 2);
+}
+
+TEST(VectorTest, EraseRemovesElement) {
+  s21::vector<int> victor{1, 2, 3};
+  victor.erase(victor.begin() + 1);
+  EXPECT_EQ(victor.size(), 2);
+  EXPECT_EQ(victor[1], 3);
+}
+
+TEST(VectorTest, SwapExchangesData) {
+  s21::vector<int> firstVec{1, 2};
+  s21::vector<int> secVec{3, 4, 5};
+  firstVec.swap(secVec);
+  EXPECT_EQ(firstVec.size(), 3);
+  EXPECT_EQ(secVec.size(), 2);
+  EXPECT_EQ(firstVec[0], 3);
+  EXPECT_EQ(secVec[0], 1);
+}
+
+TEST(VectorIteratorTest, DereferenceOperator) {
+  s21::vector<int> vec = {10, 20, 30};
+  auto it = vec.begin();
+  EXPECT_EQ(*it, 10);
+}
+
+TEST(VectorIteratorTest, IncrementOperators) {
+  s21::vector<int> vec = {1, 2, 3};
+  auto it = vec.begin();
+
+  ++it;
+  EXPECT_EQ(*it, 2);
+
+  it++;
+  EXPECT_EQ(*it, 3);
+}
+
+TEST(VectorIteratorTest, DecrementOperators) {
+  s21::vector<int> vec = {5, 6, 7};
+  auto it = vec.end();
+  --it;
+  EXPECT_EQ(*it, 7);
+
+  it--;
+  EXPECT_EQ(*it, 6);
+}
+
+TEST(VectorIteratorTest, EqualityOperators) {
+  s21::vector<int> vec = {100, 200};
+  auto it1 = vec.begin();
+  auto it2 = vec.begin();
+  auto it3 = vec.begin() + 1;
+
+  EXPECT_TRUE(it1 == it2);
+  EXPECT_FALSE(it1 == it3);
+  EXPECT_TRUE(it1 != it3);
+}
+
+TEST(VectorIteratorTest, SubscriptOperator) {
+  s21::vector<int> vec = {3, 6, 9};
+  auto it = vec.begin();
+  EXPECT_EQ(it[0], 3);
+  EXPECT_EQ(it[1], 6);
+  EXPECT_EQ(it[2], 9);
+}
+
+TEST(VectorIteratorTest, ArithmeticOperators) {
+  s21::vector<int> vec = {1, 2, 3, 4, 5};
+  auto it = vec.begin();
+  auto it2 = it + 3;
+  EXPECT_EQ(*it2, 4);
+
+  auto it3 = it2 - 2;
+  EXPECT_EQ(*it3, 2);
+
+  EXPECT_EQ(it3 - it, 1);
+}
+
+TEST(VectorIteratorTest, RelationalOperators) {
+  s21::vector<int> vec = {1, 2, 3};
+  auto it1 = vec.begin();
+  auto it2 = vec.begin() + 2;
+
+  EXPECT_TRUE(it2 > it1);
+  EXPECT_TRUE(it1 < it2);
+  EXPECT_TRUE(it2 >= it1);
+  EXPECT_TRUE(it1 <= it2);
+  EXPECT_TRUE(it1 <= it1);
+  EXPECT_TRUE(it1 >= it1);
+}
+
+TEST(VectorConstIteratorTest, DereferenceOperator) {
+  const s21::vector<int> vec = {10, 20, 30};
+  auto it = vec.cbegin();
+  EXPECT_EQ(*it, 10);
+}
+
+TEST(VectorConstIteratorTest, IncrementOperators) {
+  const s21::vector<int> vec = {1, 2, 3};
+  auto it = vec.cbegin();
+
+  ++it;
+  EXPECT_EQ(*it, 2);
+
+  it++;
+  EXPECT_EQ(*it, 3);
+}
+
+TEST(VectorConstIteratorTest, DecrementOperators) {
+  const s21::vector<int> vec = {5, 6, 7};
+  auto it = vec.cend();
+  --it;
+  EXPECT_EQ(*it, 7);
+
+  it--;
+  EXPECT_EQ(*it, 6);
+}
+
+TEST(VectorConstIteratorTest, EqualityOperators) {
+  const s21::vector<int> vec = {100, 200};
+  auto it1 = vec.cbegin();
+  auto it2 = vec.cbegin();
+  auto it3 = vec.cbegin() + 1;
+
+  EXPECT_TRUE(it1 == it2);
+  EXPECT_FALSE(it1 == it3);
+  EXPECT_TRUE(it1 != it3);
+}
+
+TEST(VectorConstIteratorTest, SubscriptOperator) {
+  const s21::vector<int> vec = {3, 6, 9};
+  auto it = vec.cbegin();
+  EXPECT_EQ(it[0], 3);
+  EXPECT_EQ(it[1], 6);
+  EXPECT_EQ(it[2], 9);
+}
+
+TEST(VectorConstIteratorTest, ArithmeticOperators) {
+  const s21::vector<int> vec = {1, 2, 3, 4, 5};
+  auto it = vec.cbegin();
+  auto it2 = it + 3;
+  EXPECT_EQ(*it2, 4);
+
+  auto it3 = it2 - 2;
+  EXPECT_EQ(*it3, 2);
+
+  EXPECT_EQ(it3 - it, 1);
+}
+
+TEST(VectorConstIteratorTest, RelationalOperators) {
+  const s21::vector<int> vec = {1, 2, 3};
+  auto it1 = vec.cbegin();
+  auto it2 = vec.cbegin() + 2;
+
+  EXPECT_TRUE(it2 > it1);
+  EXPECT_TRUE(it1 < it2);
+  EXPECT_TRUE(it2 >= it1);
+  EXPECT_TRUE(it1 <= it2);
+  EXPECT_TRUE(it1 <= it1);
+  EXPECT_TRUE(it1 >= it1);
 }
